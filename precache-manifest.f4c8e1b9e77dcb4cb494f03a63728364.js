babd1332815f7d366f8265436fcf6397f6113979
jeevi-mouli <jeev2588@gmail.com>
2020-01-14 18:42:41 +0530

    Updates

---
+++ precache-manifest.f4c8e1b9e77dcb4cb494f03a63728364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fbdd715c338ab1f158f80aeeb1d6b1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "35388a6f9128b7dde2a1",
+    "url": "/calculator/static/css/main.bdd4212c.chunk.css"
+  },
+  {
+    "revision": "6ea8b8b192cf490aa6f5",
+    "url": "/calculator/static/js/2.25f95c5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.25f95c5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35388a6f9128b7dde2a1",
+    "url": "/calculator/static/js/main.eb8c0b69.chunk.js"
+  },
+  {
+    "revision": "d00cbffd45b2d60747eb",
+    "url": "/calculator/static/js/runtime-main.38edb329.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
